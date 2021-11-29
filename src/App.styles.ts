0df60c7643bfa0f4bc2357a0bb24cd09796e12b6
Alice Ribeiro <aliceoliveiraribeiro@gmail.com>
2021-11-28 22:57:13 -0300

    feat: :sparkles: monta tela principal

---
+++ src/App.styles.ts
@@ -0,0 +1,24 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+  height: 100vh;
+`;
+
+export const Header = styled.div`
+  background: linear-gradient(225.34deg, #ff7f7f 0.56%, #7fadff 101.17%);
+  height: 150px;
+  text-align: center;
+`;
+
+export const HeaderText = styled.h1`
+  margin: 0;
+  padding: 0;
+  color: #fff;
+  padding-top: 30px;
+`;
+
+export const Body = styled.div`
+  max-width: 980px;
+  margin: auto;
+  margin-bottom: 50px;
+`;
