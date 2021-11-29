bb156e6c9f2074caf81c58447feb056b7cce370e
Alice Ribeiro <aliceoliveiraribeiro@gmail.com>
2021-11-28 22:31:00 -0300

    feat: :sparkles: cria dados para serem consumidos

---
+++ src/data/categories.ts
@@ -0,0 +1,7 @@
+import { Category } from "../types/Category";
+
+export const categories: Category = {
+  food: { title: "Alimentação", color: "#2d9cdb", expense: true },
+  rent: { title: "Aluguel", color: "#bb6bd9", expense: true },
+  salary: { title: "Salário", color: "#27ae60", expense: false }
+};
